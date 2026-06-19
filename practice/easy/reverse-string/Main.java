d30aa720e531fe101d1fc9d71cc041a396520376
Bhoomi Gupta <bhoomigupta265@gmail.com>
2026-06-19 23:36:49 +0530

    [C2G Practice] Solved: Reverse String

---
+++ practice/easy/reverse-string/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       String s=sc.nextLine();
+        String rev="";
+        
+        for(int i=s.length-1;i>=0;i--){
+            char ch=s.charAt(i);
+            rev=rev+ch;
+            
+        }
+        System.out.println(rev);
+    }
+}
