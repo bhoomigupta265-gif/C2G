fb862d15c3da7700ff1a05577fc18d13209ecf3e
Bhoomi Gupta <bhoomigupta265@gmail.com>
2026-06-19 23:41:00 +0530

    [C2G Practice] Solved: Reverse String

--- practice/easy/reverse-string/Main.java
+++ practice/easy/reverse-string/Main.java
@@ -6,10 +6,9 @@ public class Main {
        String s=sc.nextLine();
         String rev="";
         
-        for(int i=s.length-1;i>=0;i--){
+        for(int i=s.length()-1;i>=0;i--){
             char ch=s.charAt(i);
-            rev=rev+ch;
-            
+            rev=rev+ch;    
         }
         System.out.println(rev);
     }
