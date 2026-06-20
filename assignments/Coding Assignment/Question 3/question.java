e4e4de093570e8b22f4de1313fd7136573c0a300
Bhoomi Gupta <bhoomigupta265@gmail.com>
2026-06-20 22:17:14 +0530

    [C2G] Submit: Coding Assignment - Question 3

---
+++ assignments/Coding Assignment/Question 3/question.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n>=30){
+            System.out.println("Fan ON");
+        }else{
+            System.out.println("Fan OFF");
+        }
+        
+    }
+}
